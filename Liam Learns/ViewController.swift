5d02fa508af755d3ba1f65163bb075a8284fd74c
ikgood <isaackgood@gmail.com>
2018-04-03 12:23:51 -0400

    Initial Commit

---
+++ Liam Learns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Liam Learns
+//
+//  Created by Isaac Good on 4/3/18.
+//  Copyright © 2018 IKGOOD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
